338bc19f11b9bee64e5e8adf3a5932ca95cce58a
mak271 <72809131+mak271@users.noreply.github.com>
2025-10-27 14:44:44 +0300

    auth, register handler

--- go-adv-demo/internal/auth/handler.go
+++ go-adv-demo/internal/auth/handler.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"go/adv-demo/configs"
+	"go/adv-demo/pkg/request"
 	"go/adv-demo/pkg/res"
 	"net/http"
 )
@@ -24,18 +25,30 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 }
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(handler.Config.Auth.Secret)
-		fmt.Println("Login")
+	return func(w http.ResponseWriter, req *http.Request) {
+		// Read body
+		payload, err := request.HandleBody[LoginRequest](w, req)
+		if err != nil {
+			return
+		}
+		fmt.Println(payload)
 		data := LoginResponse{
 			Token: "123",
 		}
-		res.Json(w, data, http.StatusAccepted)
+		res.Json(w, data, http.StatusOK)
 	}
 }
 
 func (handler *AuthHandler) Register() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Register")
+	return func(w http.ResponseWriter, req *http.Request) {
+		payload, err := request.HandleBody[RegisterRequest](w, req)
+		if err != nil {
+			return
+		}
+		fmt.Println(payload)
+		data := RegisterResponse{
+			Token: "321",
+		}
+		res.Json(w, data, http.StatusOK)
 	}
 }
---
+++ go-adv-demo/pkg/request/handle.go
@@ -0,0 +1,21 @@
+package request
+
+import (
+	"go/adv-demo/pkg/res"
+	"net/http"
+)
+
+func HandleBody[T any](w http.ResponseWriter, r http.Request) (*T, error) {
+	body, err := Decode[T](r.Body)
+	if err != nil {
+		res.Json(w, err.Error(), http.StatusBadRequest)
+		return nil, err
+	}
+
+	err = IsValid(body)
+	if err != nil {
+		res.Json(w, err.Error(), http.StatusBadRequest)
+		return nil, err
+	}
+	return &body, nil
+}
